4b341069d154873216ced6843fd01f6c4040a195
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-03-31 22:54:08 +0200

    remove unused headers and implementation files for add, commit, init, log, and main functionalities

--- include/add.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADD_H
-#define ADD_H
-#include <string>
-
-void addFileToTracking(const std::string& fileName);
-
-#endif
--- include/commit.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMIT_H
-#define COMMIT_H
-#include <string>
-
-void commitChanges(const std::string& message);
-
-#endif
